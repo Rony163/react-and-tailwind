df2941d4e2a1b9a5c85e44e67bc9748ea6e95b12
Rony163 <minhajulislamrony43@gmail.com>
2021-09-28 19:19:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="bg-yellow-200 text-4xl hover:text-green-400">This is my text</p>
+      <p className="text-primary">My name is rony</p>
+      <p className="text-custom">Hello</p>
     </div>
   );
 }
